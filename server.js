bef0ce30e89636fe595f2cbd232622035db70d77
arunnimad <30727143+arunnimad@users.noreply.github.com>
2017-08-17 09:23:19 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,13 +18,40 @@ var content = {
             </p>`
 };
 
+function createTemplate(data) {
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+    var htmlTemplate = `<html>
+        <head>
+            <title>${title}</title>
+        </head>
+        
+        <body>
+            <div><a href="/">Home</a></div>
+            <hr/>
+            
+            <h3>${heading}</h3>
+            
+            <div>${date}</div>
+            
+            <div>
+                ${content}
+            </div>
+        </body>
+    </html>`;
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-  //res.send('Article one requested will be served here.');
+    res.send(createTemplate(articleOne));
+    //res.send('Article one requested will be served here.');
 });
 
 app.get('/article-two', function (req, res) {
