bcd256f8d74a7a4f087a25bac93d84d86d4ad99b
arunnimad <30727143+arunnimad@users.noreply.github.com>
2017-08-16 23:33:41 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content = {
+    title: 'Article One',
+    heading: 'Article One',
+    date: 'Aug 16th',
+    content: ''
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
