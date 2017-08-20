db2fbbf47e078dda8f8d5f08ef3d0cf444569062
arunnimad <30727143+arunnimad@users.noreply.github.com>
2017-08-20 11:01:46 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.get('/counter', function (req, res) {
 
 var names = [];
 app.get('/submit-name', function (req, res) {
-  var name = req.query.params.name;
+  var name = req.query.name;
   
   names.push(name);
   res.send(JSON.stringify(names));
