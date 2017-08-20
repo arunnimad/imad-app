a8e81a15ba9320e84541b9c9b5a26f4a8f4630ec
arunnimad <30727143+arunnimad@users.noreply.github.com>
2017-08-20 11:18:12 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,14 +27,27 @@ var name = nameInput.value;
 var submit = document.getElementById('submit.btn');
 submit.onclick = function () {
     
-    var names = ['name1', 'name2', 'name3', 'name4'];
+    var request = new XMLHttpRequest();
     
-    var list = '';
-    
-    for (var i = 0; i < names.length; i++) {
-        list += '<li>'+ names[i] + '</li>';
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var names = request.responseText;
+                notes = JSON.parse(notes);
+                
+                var list = '';
+                
+                for (var i = 0; i < names.length; i++) {
+                    list += '<li>'+ names[i] + '</li>';
+                }
+                
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
     }
     
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    //Make the request
+    request.open('GET', 'http://arun22772742.imad.hasura-app.io/submit-name?name='+name, true);
+    request.send(null);
 };
