cd8e414157cff30fd112b11d8b69a28dffb5dc31
arunnimad <30727143+arunnimad@users.noreply.github.com>
2017-08-26 10:20:12 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -144,7 +144,7 @@ function createTemplate(data) {
             
             <h3>${heading}</h3>
             
-            <div>${date}</div>
+            <div>${date.toDateString()}</div>
             
             <div>
                 ${content}
