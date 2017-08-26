d4e862511ebb9fdc9eed6d979f1656eb4e89313a
arunnimad <30727143+arunnimad@users.noreply.github.com>
2017-08-26 13:15:03 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/login', function (req, res) {
         if (err) {
             res.status(500).send(err.toString());
         }else {
-            if (result.rows.length === 0) {res.send('errror1');
+            if (result.rows.length === 0) {
                 res.send('username/password is invalid');
             }else {res.send('errror3');
                 var dbString = result.rows[0].password;
